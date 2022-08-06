9beef6e00c143f4125d20fc4c1012ed875e9942b
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-08-06 09:49:24 -0300

    Tecnologia Java e Orientação a Objetos - Feature 01

---
+++ src/main/java/br/edu/infnet/appempenho/EmpenhoTeste.java
@@ -0,0 +1,40 @@
+package br.edu.infnet.appempenho;
+
+import java.time.LocalDateTime;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import br.edu.infnet.model.domain.Empenho;
+
+@Component
+@Order(1)
+public class EmpenhoTeste implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		System.out.println("#empenho");
+		
+		Empenho empenho1 = new Empenho();
+		empenho1.setNumero(476);
+		empenho1.setData(LocalDateTime.now());
+		empenho1.setValor(5000);		
+		System.out.println(empenho1);	
+		
+		Empenho empenho2 = new Empenho();
+		empenho2.setNumero(843);
+		empenho2.setData(LocalDateTime.now());
+		empenho2.setValor(6500);		
+		System.out.println(empenho2);		
+
+		Empenho empenho3 = new Empenho();
+		empenho3.setNumero(974);
+		empenho3.setData(LocalDateTime.now());
+		empenho3.setValor(2700);		
+		System.out.println(empenho3);		
+	}
+
+}
--- src/main/java/br/edu/infnet/model/domain/Empenho.java
+++ src/main/java/br/edu/infnet/model/domain/Empenho.java
@@ -1,5 +1,40 @@
 package br.edu.infnet.model.domain;
 
+import java.time.LocalDateTime;
+
 public class Empenho {
+	
+	private int numero;
+	private LocalDateTime data;
+	private float valor;
+	
+	public int getNumero() {
+		return numero;
+	}
+	
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+	
+	public LocalDateTime getData() {
+		return data;
+	}
+	
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+	
+	public float getValor() {
+		return valor;
+	}
+	
+	public void setValor(float valor) {
+		this.valor = valor;
+	}
+	
+	@Override
+	public String toString() {
+		return numero + ";" + data + ";" + valor;
+	}	
 
 }
