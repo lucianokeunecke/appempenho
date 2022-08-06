62ca21cd039632a7da07c2c1def85b1119c39c6c
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-08-06 12:05:42 -0300

    Tecnologia Java e Orientação a Objetos - Feature 02

--- src/main/java/br/edu/infnet/appempenho/ConcorrenciaTeste.java
+++ src/main/java/br/edu/infnet/appempenho/ConcorrenciaTeste.java
@@ -23,27 +23,27 @@ public class ConcorrenciaTeste implements ApplicationRunner {
 		concorrencia1.setNumero(150);
 		concorrencia1.setDataExpedicao(LocalDateTime.now());
 		concorrencia1.setObjetoLicitacao("Contratação de empresa especializada para execução de serviço de capeamento asfáltico");
-		concorrencia1.eletronica = true;
-		concorrencia1.prazoExecucao = "30 dias";
-		concorrencia1.valorMinimoCapital = 500000;		
+		concorrencia1.setEletronica(true);
+		concorrencia1.setPrazoExecucao("30 dias");
+		concorrencia1.setValorMinimoCapital(500000);		
 		System.out.println(concorrencia1);	
 		
 		Concorrencia concorrencia2 = new Concorrencia();
 		concorrencia2.setNumero(153);
 		concorrencia2.setDataExpedicao(LocalDateTime.now());
 		concorrencia2.setObjetoLicitacao("Contratação de empresa especializada para execução de serviço de engenharia");
-		concorrencia2.eletronica = false;
-		concorrencia2.prazoExecucao = "45 dias";
-		concorrencia2.valorMinimoCapital = 700000;		
+		concorrencia2.setEletronica(false);
+		concorrencia2.setPrazoExecucao("45 dias");
+		concorrencia2.setValorMinimoCapital(700000);		
 		System.out.println(concorrencia2);	
 		
 		Concorrencia concorrencia3 = new Concorrencia();
 		concorrencia3.setNumero(150);
 		concorrencia3.setDataExpedicao(LocalDateTime.now());
 		concorrencia3.setObjetoLicitacao("Contratação de empresa especializada para execução de obra");
-		concorrencia3.eletronica = true;
-		concorrencia3.prazoExecucao = "1 ano e 2 meses";
-		concorrencia3.valorMinimoCapital = 1000000;		
+		concorrencia3.setEletronica(true);
+		concorrencia3.setPrazoExecucao("1 ano e 2 meses");
+		concorrencia3.setValorMinimoCapital(1000000);		
 		System.out.println(concorrencia3);	
 		
 	}
--- src/main/java/br/edu/infnet/appempenho/model/domain/Concorrencia.java
+++ src/main/java/br/edu/infnet/appempenho/model/domain/Concorrencia.java
@@ -2,10 +2,34 @@ package br.edu.infnet.appempenho.model.domain;
 
 public class Concorrencia extends ProcessoLicitatorio {
 	
-	public boolean eletronica;
-	public String prazoExecucao;
-	public float valorMinimoCapital;	
+	private boolean eletronica;
+	private String prazoExecucao;
+	private float valorMinimoCapital;		
 	
+	public boolean isEletronica() {
+		return eletronica;
+	}
+
+	public void setEletronica(boolean eletronica) {
+		this.eletronica = eletronica;
+	}
+
+	public String getPrazoExecucao() {
+		return prazoExecucao;
+	}
+
+	public void setPrazoExecucao(String prazoExecucao) {
+		this.prazoExecucao = prazoExecucao;
+	}
+
+	public float getValorMinimoCapital() {
+		return valorMinimoCapital;
+	}
+
+	public void setValorMinimoCapital(float valorMinimoCapital) {
+		this.valorMinimoCapital = valorMinimoCapital;
+	}
+
 	@Override
 	public String toString() {
 		return eletronica + ";" + prazoExecucao + ";" + valorMinimoCapital + super.toString();
