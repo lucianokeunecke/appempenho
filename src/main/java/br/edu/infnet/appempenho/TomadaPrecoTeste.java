3b744620c7d7712032f9a23b629e5c608eb0c87c
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-08-06 12:43:13 -0300

    Tecnologia Java e Orientação a Objetos - Feature 02

--- src/main/java/br/edu/infnet/appempenho/TomadaPrecoTeste.java
+++ src/main/java/br/edu/infnet/appempenho/TomadaPrecoTeste.java
@@ -23,27 +23,27 @@ public class TomadaPrecoTeste implements ApplicationRunner {
 		tomadaPreco1.setNumero(750);
 		tomadaPreco1.setDataExpedicao(LocalDateTime.now());
 		tomadaPreco1.setObjetoLicitacao("Contratação de empresa especializada em serviço de limpeza");
-		tomadaPreco1.permiteSubcontratacao = false;
-		tomadaPreco1.condicaoPagamento = "30/60/90 dias";
-		tomadaPreco1.valorEstimadoDotacaoOrcamentaria = 150000;		
+		tomadaPreco1.setPermiteSubcontratacao(false);
+		tomadaPreco1.setCondicaoPagamento("30/60/90 dias");
+		tomadaPreco1.setValorEstimadoDotacaoOrcamentaria(150000);		
 		System.out.println(tomadaPreco1);		
 		
 		TomadaPreco tomadaPreco2 = new TomadaPreco();
 		tomadaPreco2.setNumero(755);
 		tomadaPreco2.setDataExpedicao(LocalDateTime.now());
 		tomadaPreco2.setObjetoLicitacao("Contratação de exames laboratoriais");
-		tomadaPreco2.permiteSubcontratacao = true;
-		tomadaPreco2.condicaoPagamento = "15/30/45/60 dias";
-		tomadaPreco2.valorEstimadoDotacaoOrcamentaria = 350000;		
+		tomadaPreco2.setPermiteSubcontratacao(true);
+		tomadaPreco2.setCondicaoPagamento("15/30/45/60 dias");
+		tomadaPreco2.setValorEstimadoDotacaoOrcamentaria(350000);		
 		System.out.println(tomadaPreco2);		
 		
 		TomadaPreco tomadaPreco3 = new TomadaPreco();
 		tomadaPreco3.setNumero(796);
 		tomadaPreco3.setDataExpedicao(LocalDateTime.now());
 		tomadaPreco3.setObjetoLicitacao("Contratação de empresa especializada em serviços de contrução civil");
-		tomadaPreco3.permiteSubcontratacao = false;
-		tomadaPreco3.condicaoPagamento = "45/90 dias";
-		tomadaPreco3.valorEstimadoDotacaoOrcamentaria = 175000;		
+		tomadaPreco3.setPermiteSubcontratacao(false);
+		tomadaPreco3.setCondicaoPagamento("45/90 dias");
+		tomadaPreco3.setValorEstimadoDotacaoOrcamentaria(175000);		
 		System.out.println(tomadaPreco3);		
 		
 	}
--- src/main/java/br/edu/infnet/appempenho/model/domain/TomadaPreco.java
+++ src/main/java/br/edu/infnet/appempenho/model/domain/TomadaPreco.java
@@ -2,10 +2,34 @@ package br.edu.infnet.appempenho.model.domain;
 
 public class TomadaPreco extends ProcessoLicitatorio {
 	
-	public boolean permiteSubcontratacao;
-	public String condicaoPagamento;
-	public float valorEstimadoDotacaoOrcamentaria;	
+	private boolean permiteSubcontratacao;
+	private String condicaoPagamento;
+	private float valorEstimadoDotacaoOrcamentaria;	
 	
+	public boolean isPermiteSubcontratacao() {
+		return permiteSubcontratacao;
+	}
+
+	public void setPermiteSubcontratacao(boolean permiteSubcontratacao) {
+		this.permiteSubcontratacao = permiteSubcontratacao;
+	}
+
+	public String getCondicaoPagamento() {
+		return condicaoPagamento;
+	}
+
+	public void setCondicaoPagamento(String condicaoPagamento) {
+		this.condicaoPagamento = condicaoPagamento;
+	}
+
+	public float getValorEstimadoDotacaoOrcamentaria() {
+		return valorEstimadoDotacaoOrcamentaria;
+	}
+
+	public void setValorEstimadoDotacaoOrcamentaria(float valorEstimadoDotacaoOrcamentaria) {
+		this.valorEstimadoDotacaoOrcamentaria = valorEstimadoDotacaoOrcamentaria;
+	}
+
 	@Override
 	public String toString() {
 		return permiteSubcontratacao + ";" + condicaoPagamento + ";" + valorEstimadoDotacaoOrcamentaria + super.toString();
