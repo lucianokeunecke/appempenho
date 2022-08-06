8b67fd906709c56401c6fd0662ad75df9fa86310
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-08-06 10:10:55 -0300

    Tecnologia Java e Orientação a Objetos - Feature 01

---
+++ src/main/java/br/edu/infnet/appempenho/FornecedorTeste.java
@@ -0,0 +1,39 @@
+package br.edu.infnet.appempenho;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import br.edu.infnet.model.domain.Fornecedor;
+
+@Component
+@Order(2)
+public class FornecedorTeste implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+		System.out.println("#fornecedor");
+		
+		Fornecedor fornecedor1 = new Fornecedor();
+		fornecedor1.setCodigo(15);
+		fornecedor1.setNome("Luciano Keunecke");
+		fornecedor1.setCnpjCpf(3189734992L);		
+		System.out.println(fornecedor1);	
+		
+		Fornecedor fornecedor2 = new Fornecedor();
+		fornecedor2.setCodigo(96);
+		fornecedor2.setNome("Governança Brasil");
+		fornecedor2.setCnpjCpf(4983932000152L);		
+		System.out.println(fornecedor2);
+
+		Fornecedor fornecedor3 = new Fornecedor();
+		fornecedor3.setCodigo(175);
+		fornecedor3.setNome("Eletrônica Blumenau");
+		fornecedor3.setCnpjCpf(78642321000192L);		
+		System.out.println(fornecedor3);		
+		
+	}
+
+}
--- src/main/java/br/edu/infnet/model/domain/Fornecedor.java
+++ src/main/java/br/edu/infnet/model/domain/Fornecedor.java
@@ -1,5 +1,38 @@
 package br.edu.infnet.model.domain;
 
 public class Fornecedor {
+	
+	private int codigo;
+	private String nome;
+	private long cnpjCpf;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public long getCnpjCpf() {
+		return cnpjCpf;
+	}
+
+	public void setCnpjCpf(long cnpjCpf) {
+		this.cnpjCpf = cnpjCpf;
+	}
+
+	@Override
+	public String toString() {
+		return codigo + ";" + nome + ";" + cnpjCpf;
+	}		
 
 }
