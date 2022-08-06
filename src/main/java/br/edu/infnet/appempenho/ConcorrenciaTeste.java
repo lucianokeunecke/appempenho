1d3692a2aa46efd27b3677741ab926ba7dc57e49
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-08-06 10:17:04 -0300

    Tecnologia Java e Orientação a Objetos - Feature 01

---
+++ src/main/java/br/edu/infnet/appempenho/ConcorrenciaTeste.java
@@ -0,0 +1,39 @@
+package br.edu.infnet.appempenho;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import br.edu.infnet.model.domain.Concorrencia;
+
+@Component
+@Order(3)
+public class ConcorrenciaTeste implements ApplicationRunner {
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+		System.out.println("#concorrencia");
+		
+		Concorrencia concorrencia1 = new Concorrencia();
+		concorrencia1.setEletronica(true);
+		concorrencia1.setPrazoExecucao("30 dias");
+		concorrencia1.setValorMinimoCapital(500000);		
+		System.out.println(concorrencia1);	
+		
+		Concorrencia concorrencia2 = new Concorrencia();
+		concorrencia2.setEletronica(false);
+		concorrencia2.setPrazoExecucao("45 dias");
+		concorrencia2.setValorMinimoCapital(700000);		
+		System.out.println(concorrencia2);	
+		
+		Concorrencia concorrencia3 = new Concorrencia();
+		concorrencia3.setEletronica(true);
+		concorrencia3.setPrazoExecucao("2 meses");
+		concorrencia3.setValorMinimoCapital(1000000);		
+		System.out.println(concorrencia3);	
+		
+	}
+
+}
--- src/main/java/br/edu/infnet/model/domain/Concorrencia.java
+++ src/main/java/br/edu/infnet/model/domain/Concorrencia.java
@@ -1,5 +1,38 @@
 package br.edu.infnet.model.domain;
 
 public class Concorrencia {
+	
+	private boolean eletronica;
+	private String prazoExecucao;
+	private float valorMinimoCapital;
+	
+	public boolean isEletronica() {
+		return eletronica;
+	}
+	
+	public void setEletronica(boolean eletronica) {
+		this.eletronica = eletronica;
+	}
+	
+	public String getPrazoExecucao() {
+		return prazoExecucao;
+	}
+	
+	public void setPrazoExecucao(String prazoExecucao) {
+		this.prazoExecucao = prazoExecucao;
+	}
+	
+	public float getValorMinimoCapital() {
+		return valorMinimoCapital;
+	}
+	
+	public void setValorMinimoCapital(float valorMinimoCapital) {
+		this.valorMinimoCapital = valorMinimoCapital;
+	}
+	
+	@Override
+	public String toString() {
+		return eletronica + ";" + prazoExecucao + ";" + valorMinimoCapital;
+	}		
 
 }
