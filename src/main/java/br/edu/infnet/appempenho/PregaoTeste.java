244073d85821696a6e5623499ccf869f4d881914
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-08-27 08:06:45 -0300

    Tecnologia Java e Orientação a Objetos - Feature 08

--- src/main/java/br/edu/infnet/appempenho/PregaoTeste.java
+++ src/main/java/br/edu/infnet/appempenho/PregaoTeste.java
@@ -1,5 +1,9 @@
 package br.edu.infnet.appempenho;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.time.LocalDateTime;
 
 import org.springframework.boot.ApplicationArguments;
@@ -20,81 +24,52 @@ public class PregaoTeste implements ApplicationRunner {
 		
 		System.out.println("====================================================================================================================");
 		
-		try {
-			Pregao pregao1 = new Pregao();
-			pregao1.setNumero(356);
-			pregao1.setDataExpedicao(LocalDateTime.now());
-			pregao1.setObjetoLicitacao("Contratação de materiais de consumo");
-			pregao1.setValorEstimadoEdital(550000);
-			pregao1.setRegistroPreco(true);
-			pregao1.setCriterioJulgamento("Menor Preço");
-			pregao1.setIndiceReajuste(1.5f);		
-			System.out.println("Valor Estimado do Edital R$ " + pregao1.calcularValorEstimadoEdital());
-			PregaoController.incluir(pregao1);
-		} catch (IndiceReajusteZeradoNegativoException e) {
-			System.out.println("[ERROR - PREGAO] " + e.getMessage());
-		}
+		String diretorio = "D:/Keunecke/INFNET/Modulo 03/appempenho/src/main/resources/arquivos/";
+		
+		String arquivo = "pregao.txt";
 		
 		try {
-			Pregao pregao2 = new Pregao();
-			pregao2.setNumero(376);
-			pregao2.setDataExpedicao(LocalDateTime.now());
-			pregao2.setObjetoLicitacao("Contratação de materiais de expediente");
-			pregao2.setValorEstimadoEdital(750000);
-			pregao2.setRegistroPreco(false);
-			pregao2.setCriterioJulgamento("Maior Desconto");
-			pregao2.setIndiceReajuste(2.35f);		
-			System.out.println("Valor Estimado do Edital R$ " + pregao2.calcularValorEstimadoEdital());
-			PregaoController.incluir(pregao2);
-		} catch (IndiceReajusteZeradoNegativoException e) {
-			System.out.println("[ERROR - PREGAO] " + e.getMessage());
+			try {
+				FileReader fileReader = new FileReader(diretorio + arquivo);
+				
+				BufferedReader leitura = new BufferedReader(fileReader); 
+				
+				String linha = leitura.readLine();
+				
+				while(linha != null) {
+
+					try {
+						String[] campos = linha.split(";");
+						
+						Pregao pregao = new Pregao();
+						pregao.setNumero(Integer.parseInt(campos[3]));
+						pregao.setDataExpedicao(LocalDateTime.now());
+						pregao.setObjetoLicitacao(campos[5]);
+						pregao.setValorEstimadoEdital(550000);
+						pregao.setRegistroPreco(Boolean.parseBoolean(campos[0]));
+						pregao.setCriterioJulgamento(campos[1]);
+						pregao.setIndiceReajuste(Float.parseFloat(campos[2]));		
+						System.out.println("Valor Estimado do Edital R$ " + pregao.calcularValorEstimadoEdital());
+						PregaoController.incluir(pregao);
+					} catch (IndiceReajusteZeradoNegativoException e) {
+						System.out.println("[ERROR - PREGAO] " + e.getMessage());
+					}
+					
+					linha = leitura.readLine();
+				}
+				
+				leitura.close();
+				
+				fileReader.close();
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("[ERRO] O arquivo não existe");
+			} catch (IOException e) {
+				System.out.println("[ERRO] Problema no fechamento do arquivo");
+			}
+		} finally {
+			System.out.println("Terminou!!!");
 		}
-		
-		try {
-			Pregao pregao3 = new Pregao();
-			pregao3.setNumero(397);
-			pregao3.setDataExpedicao(LocalDateTime.now());
-			pregao3.setObjetoLicitacao("Contratação de materiais de escritório");
-			pregao3.setValorEstimadoEdital(850000);
-			pregao3.setRegistroPreco(false);
-			pregao3.setCriterioJulgamento("Maior Lance ou Oferta");
-			pregao3.setIndiceReajuste(0.75f);		
-			System.out.println("Valor Estimado do Edital R$ " + pregao3.calcularValorEstimadoEdital());
-			PregaoController.incluir(pregao3);
-		} catch (IndiceReajusteZeradoNegativoException e) {
-			System.out.println("[ERROR - PREGAO] " + e.getMessage());
-		}
-		
-		try {
-			Pregao pregao4 = new Pregao();
-			pregao4.setNumero(397);
-			pregao4.setDataExpedicao(LocalDateTime.now());
-			pregao4.setObjetoLicitacao("Contratação de materiais de escritório");
-			pregao4.setValorEstimadoEdital(850000);
-			pregao4.setRegistroPreco(false);
-			pregao4.setCriterioJulgamento("Maior Lance ou Oferta");
-			//pregao4.setIndiceReajuste(1f);		
-			System.out.println("Valor Estimado do Edital R$ " + pregao4.calcularValorEstimadoEdital());
-			PregaoController.incluir(pregao4);
-		} catch (IndiceReajusteZeradoNegativoException e) {
-			System.out.println("[ERROR - PREGAO] " + e.getMessage());
-		}
-		
-		try {
-			Pregao pregao5 = new Pregao();
-			pregao5.setNumero(397);
-			pregao5.setDataExpedicao(LocalDateTime.now());
-			pregao5.setObjetoLicitacao("Contratação de materiais de escritório");
-			pregao5.setValorEstimadoEdital(850000);
-			pregao5.setRegistroPreco(false);
-			pregao5.setCriterioJulgamento("Maior Lance ou Oferta");
-			pregao5.setIndiceReajuste(-0.05f);		
-			System.out.println("Valor Estimado do Edital R$ " + pregao5.calcularValorEstimadoEdital());
-			PregaoController.incluir(pregao5);
-		} catch (IndiceReajusteZeradoNegativoException e) {
-			System.out.println("[ERROR - PREGAO] " + e.getMessage());
-		}
-		
 		
 	}
 
