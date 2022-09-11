0718486d964c92715ac45bfc3f6792f60e8e0956
lucianokeunecke <47121398+lucianokeunecke@users.noreply.github.com>
2022-09-11 09:56:03 -0300

    Java Web e Injeção de Dependência - Feature 03

--- src/main/java/br/edu/infnet/appempenho/UsuarioTeste.java
+++ src/main/java/br/edu/infnet/appempenho/UsuarioTeste.java
@@ -1,5 +1,10 @@
 package br.edu.infnet.appempenho;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -13,16 +18,56 @@ public class UsuarioTeste implements ApplicationRunner {
 	
 	@Autowired
 	private UsuarioService usuarioService;
+	
+	private Usuario usuario;
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		
-		Usuario usuario = new Usuario();
-		usuario.setEmail("lucianokeunecke@gmail.com");
-		usuario.setNome("Luciano Keunecke");
-		usuario.setSenha("123456");
+		usuario = new Usuario();
+		usuario.setEmail("admin@gmail.com");
+		usuario.setNome("Administrador");
+		usuario.setSenha("654321");
 		
 		usuarioService.incluir(usuario);
+		
+		String diretorio = "D:/Keunecke/INFNET/Modulo 03/appempenho/src/main/resources/arquivos/";
+		
+		String arquivo = "Usuario.txt";
+		
+		try {
+			try {
+				FileReader fileReader = new FileReader(diretorio + arquivo);
+				
+				BufferedReader leitura = new BufferedReader(fileReader); 
+				
+				String linha = leitura.readLine();
+				
+				while(linha != null) {
+					
+					String[] campos = linha.split(";");
+					
+					usuario = new Usuario();
+					usuario.setEmail(campos[0]);
+					usuario.setNome(campos[1]);
+					usuario.setSenha(campos[2]);
+					
+					usuarioService.incluir(usuario);
+					
+					linha = leitura.readLine();
+				}
+				
+				leitura.close();
+				
+				fileReader.close();
+				
+			} catch (FileNotFoundException e) {
+				System.out.println("[ERRO] O arquivo não existe");
+			} catch (IOException e) {
+				System.out.println("[ERRO] Problema no fechamento do arquivo");
+			}
+		} finally {
+			System.out.println("Terminou!!!");
+		}
 	}
-
 }
